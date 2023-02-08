9dba0545cfa1a57c43ada20aad19597eb13084cb
j0nk1ns <97742736+j0nk1ns@users.noreply.github.com>
2023-02-08 12:01:07 -0500

    Lesson 4.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,15 @@ public class PlayerController : MonoBehaviour
 
     public bool hasPowerup;
     private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+
+       
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+         powerupIndicator.transform.position = transform.position + new Vector3 (0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other) 
@@ -33,12 +37,16 @@ public class PlayerController : MonoBehaviour
             hasPowerup = true;
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
+
+            powerupIndicator.gameObject.SetActive(true);
         }
 
      IEnumerator PowerupCountdownRoutine()
         {
             yield return new WaitForSeconds(7);
             hasPowerup = false; 
+
+            powerupIndicator.gameObject.SetActive(false);
         }
     }
 
